961f5e83b16631549d05de7e75b96b19f90ca0af
Shrikrishna Kaje <115392945+Shrikris630@users.noreply.github.com>
2025-09-07 13:23:05 -0400

    Update tt_adpll.v

--- src/tt_adpll.v
+++ src/tt_adpll.v
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: Apache-2.0 */
  
 `include "adpll_top.v"
 `include "adpll_5bit.v"
@@ -9,7 +9,7 @@
 `include "acs_5bit.v"
 `include "pi_filter_5bit.v"
 `include "dco_5bit.v"
-`include "freq_divider_5bit.v" */
+`include "freq_divider_5bit.v"
 
 `default_nettype none
 
@@ -67,5 +67,6 @@ module tt_um_adpll (
 
 
 endmodule
+
 
 
